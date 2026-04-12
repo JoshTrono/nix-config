9d7ae520037f1148da5f0aca21f5cb42ca0a6f68
Joshua Trono <Josh.Trono@gmail.com>
2026-04-12 18:46:37 -0400

    .

--- nixos/server/configuration.nix
+++ nixos/server/configuration.nix
@@ -1,6 +1,45 @@
 { config, pkgs, lib, ... }:
 
 {
+    imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # inputs.self.nixosModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+  ];
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      inputs.self.overlays.additions
+      inputs.self.overlays.modifications
+      inputs.self.overlays.unstable-packages
+      inputs.self.overlays.old-stable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
   # System basics
   system.stateVersion = "25.11";
 
