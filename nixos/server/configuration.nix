ef9615eab512db8132a595dbba9ae8119614c7b6
Joshua Trono <Josh.Trono@gmail.com>
2026-04-12 19:04:39 -0400

    :(

--- nixos/server/configuration.nix
+++ nixos/server/configuration.nix
@@ -1,6 +1,21 @@
 { inputs, config, pkgs, lib, ... }:
 
 {
+    imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # inputs.self.nixosModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+    ./common/default.nix
+  ];
  nixpkgs = {
     # You can add overlays here
     overlays = [
